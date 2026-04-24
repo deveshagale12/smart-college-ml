3bae3e9a5d3cce69f3d031bd3545f74fb9b2aadb
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-04-24 14:00:30 +0530

    main

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 # Enable CORS so your Spring Boot or React app can call this live
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # Allows your local file to talk to the server
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
